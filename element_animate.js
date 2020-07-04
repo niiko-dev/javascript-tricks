4bb3111eb0109db313b2756ae1a78db2d35c6cde
niiko-dev <67813089+niiko-dev@users.noreply.github.com>
2020-07-04 17:47:10 +0200

    Create Simple Element Animations

---
+++ element_animate.js
@@ -0,0 +1,26 @@
+Element.prototype.Animate = function (array, delay) {
+    var that = this;
+
+    array.forEach(arr => {
+        for (const item in arr) {
+            
+            setTimeout(() => {
+
+                var value = arr[item];
+    
+                console.log(item, value)
+                that.style[item] = value;
+                that.style['transitionProperty'] = `${item}`;
+                that.style['transitionDuration'] = `${delay / parseInt(array.indexOf(arr) + 1) / 1000}s`
+
+            }, delay / array.length * array.indexOf(arr) + 1)
+
+        }
+    })
+} 
+
+Usage:
+    element.Animate(
+        [{ "color": "green" }, { "color": "red" }, { "margin": "50px" }, { "margin": "0px" }]
+    , 1800)
+*/
