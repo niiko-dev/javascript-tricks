786ae227144050a1a1ba9932f7c7d8ed6c9bef8d
niiko-dev <67813089+niiko-dev@users.noreply.github.com>
2020-07-04 04:23:32 +0200

    Create element_json.js
    Create JSON in elements

---
+++ element_json.js
@@ -0,0 +1,33 @@
+Element.prototype.JSON = function () {
+    var that = this;
+
+    var config = {
+        splitChar: "﹒",
+        attribute: "json",
+        encryption: 7
+    }
+
+    return {
+        set: function (string) {
+            string = JSON.stringify(string);
+            var stringSplit = string.split("")
+            stringSplit.forEach(temp => {
+                stringSplit[stringSplit.indexOf(temp)] = temp.charCodeAt(0) * config.encryption;
+            });
+
+            stringSplit = stringSplit.join(config.splitChar);
+            that.setAttribute(config.attribute, stringSplit);
+            return stringSplit;
+        },
+        get: function () {
+            var string = that.getAttribute(config.attribute);
+            var stringSplit = string.split(config.splitChar);
+            stringSplit.forEach(temp => {
+                stringSplit[stringSplit.indexOf(temp)] = String.fromCharCode(temp / config.encryption);
+            });
+
+            stringSplit = stringSplit.join("");
+            return JSON.parse(stringSplit);
+        }
+    }
+}
